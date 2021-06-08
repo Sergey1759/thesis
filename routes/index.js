7c2b20947293128c187619bda044d4594571f65c
Sergey1759 <grt.sergey.lisitskii@gmail.com>
2021-06-08 19:19:16 +0300

    first

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/re-rent', function(req, res, next) {
+  res.render('re-rent', { title: 'Express' });
+});
+
+router.get('/to-sold', function(req, res, next) {
+  res.render('to-sold', { title: 'Express' });
+});
+
+router.get('/category', function(req, res, next) {
+  res.render('category', { title: 'Express' });
+});
+
 module.exports = router;
